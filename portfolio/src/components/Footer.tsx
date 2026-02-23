7c732198c60fa3ca2973b55e1a3722a9b2e0a0d9
Harsh Aerndolkar <harsh.aerndolkar2022@sait.ac.in>
2026-02-23 20:51:45 +0530

    final backend for certificate

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -2,7 +2,9 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-left">
-        © 2026 <a href="#">Harsh Aerndolkar</a>. Built with 💖 using React & TypeScript.
+        © {new Date().getFullYear()}{" "}
+        <a href="#">Harsh Aerndolkar</a>. Built with 💖 using React &
+        TypeScript.
       </div>
     </footer>
   );
